d09bfc448731c7c92d62632703c0caf742db6cba
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-07-20 14:47:48 -0300

    Update script.js
    js avances

--- script.js
+++ script.js
@@ -4,44 +4,38 @@ let form = document.querySelector('#form');
 let input = document.querySelector('#input');
 let msg = document.querySelector('#msg');
 let tasks = document.querySelector('#tasks');
-let taskToDo = {
+let taskList =document.querySelector('p');
 
-};
+let taskToDo = []
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("Se hizo click en el formulario");
 
-    dataValidation();
+    taskValidation();
 });
 
-let dataValidation = () => {
+let taskValidation = () => {
     if (input.value === "") {
         msg.innerHTML = "Debes agregar la menos una tarea";
         console.log("El suario no agrego ninguna tarea");
     } else {
         console.log("se ha agregado una tarea");
         msg.innerHTML = "";
-        acceptTask();
+        acceptAddTask();
 
     }
 };
 
-let acceptTask = () => {
-    ;
-    taskToDo["text"] = input.value
-    console.log(taskToDo);
-
-    createTask();
-    storageData();
-
-};
-
-let createTask = () => {
+let acceptAddTask = () => {
+    
+    const task = input.value
+    taskToDo.push(task);
+    console.log(task);
     tasks.innerHTML += `
     <div>
-    <p>${taskToDo.text}</p>
+    <p>${task}</p>
     <span class="plus_buttons"> 
     <i onClick="deleteTask(this)" class="bi bi-trash3-fill"></i>
     <i onclick="editTask(this)" class="bi bi-pencil-square"></i>
@@ -49,6 +43,7 @@ let createTask = () => {
     </div> 
     `;
     input.value = "";
+    storagetask();
 };
 
 let deleteTask = (e) => {
@@ -62,12 +57,21 @@ let editTask = (e) => {
     console.log("se esta editando una tarea")
 };
 
-let data = [];
-let storageData = () =>{
-    data.push({});
 
-    localStorage.setItem('data', JSON.stringify(data));
+let storagetask = () =>{
+
+    localStorage.setItem('taskAdded', JSON.stringify(taskToDo));
 
 console.log('Se almaceno la tarea en localStorage')
 }
+
+let getTask = () => {
+    let savedTask = localStorage.getItem('taskToDo');
+    if (savedTask) {
+        taskToDo = JSON.parse(taskToDo)
+    }
+    
+}
+
+window.addEventListener('load', getTask)
 
