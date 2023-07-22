4bb61f4697c94d7bc0ecf88071dfe3bc615fbf93
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-07-21 23:18:07 -0400

    CRUD Listo  sin comentarios
    CRUD listo para presentar sin comentarios

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-//Definicion de variables a utilizar 
-
 let form = document.querySelector('#form');
 let textInput = document.querySelector('#input');
 let textitle = document.querySelector('#title')
 let msg = document.querySelector('#msg');
 let tasks = document.querySelector('#tasks');
 
-//añadir un evento cuando el formulario de envie 
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("Se hizo click en el formulario");
     taskValidation();
 });
-
-// validacion del ingreso de los datos en el input 
 
 let taskValidation = () => {
     if (textInput.value === "" || textitle.value === "") {
@@ -27,9 +21,7 @@ let taskValidation = () => {
     }
 };
 
-// recoger los datos aceptados previamente  
-
-let taskToDo = [{}]; //almacenar objetos dentro de un arreglo 
+let taskToDo = [{}]; 
 
 let acceptTask = () => {
     taskToDo.push({
@@ -41,8 +33,6 @@ let acceptTask = () => {
     console.log(textitle.value);
     addTask();
 };
-
-// agregar los datos recolectados a un template y darle una posición para poder invocarlos //hacer que el formulario vuelva a blanco 
 
 let addTask = () => {
     tasks.innerHTML = "";
@@ -62,16 +52,12 @@ let addTask = () => {
     textitle.value = "";
 }
 
-// eliminar un elemento
-
 let deleteTask = (e) => {
     e.parentElement.parentElement.remove();
     taskToDo.splice(e.parentElement.parentElement.id, 1);
     localStorage.setItem("taskToDo", JSON.stringify(taskToDo));
     console.log("Se ha eliminado la tarea")
 };
-
-// editar tarea, eliminar la antigua, desplegar la nueva 
 
 let editTask = (e) => {
     let taskToEdit = e.parentElement.parentElement;
